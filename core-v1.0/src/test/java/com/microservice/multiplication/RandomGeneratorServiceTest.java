667452d0909e43ee525c5dfb35f531e74850855b
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-21 12:44:35 +0330

    create randomGeneratorServiceTest

---
+++ core-v1.0/src/test/java/com/microservice/multiplication/RandomGeneratorServiceTest.java
@@ -0,0 +1,32 @@
+package com.microservice.multiplication;
+
+import com.microservice.multiplication.service.RandomGeneratorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class RandomGeneratorServiceTest {
+
+    @Autowired
+    private RandomGeneratorService randomGeneratorService;
+
+    @Test
+    public void generateRandomFactorIsBetweenExpectedLimits() {
+        List<Integer> randomFactors = IntStream.range(0, 1000)
+                .map(i -> randomGeneratorService.generateRandomNumber())
+                .boxed().collect(Collectors.toList());
+
+        assertThat(randomFactors).containsOnlyElementsOf(IntStream.range(11, 100)
+                        .boxed().collect(Collectors.toList()));
+    }
+}
