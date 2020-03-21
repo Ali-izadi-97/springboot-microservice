9aef4d8da961c31c974c4c27c2bab3aca40184f2
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-21 14:29:42 +0330

    delete field result from multiplication domain

--- core-v1.0/src/main/java/com/microservice/multiplication/domain/Multiplication.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/domain/Multiplication.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class Multiplication {
-    private int factorA;
-    private int factorB;
-    private int result;
+public final class Multiplication {
+    private final int factorA;
+    private final int factorB;
 
     public Multiplication(int factorA, int factorB) {
         this.factorA = factorA;
         this.factorB = factorB;
-        this.result = factorA * factorB;
     }
 }
