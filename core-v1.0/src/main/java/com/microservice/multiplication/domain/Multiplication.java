7436e55efe38c6fb22a0a5ec378b526201a9c9a8
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-21 11:57:32 +0330

    add constructor by calculate result inside to multiplication domain

--- core-v1.0/src/main/java/com/microservice/multiplication/domain/Multiplication.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/domain/Multiplication.java
@@ -4,8 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Multiplication {
     private int factorA;
     private int factorB;
+    private int result;
+
+    public Multiplication(int factorA, int factorB) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+        this.result = factorA * factorB;
+    }
 }
