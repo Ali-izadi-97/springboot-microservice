eefb2cfdf81cc2ad35d72f77c506824842d67f1b
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-24 06:20:31 +0330

    add flag for check correctly in multiplicationResult model

--- core-v1.0/src/main/java/com/microservice/multiplication/domain/MultiplicationResult.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/domain/MultiplicationResult.java
@@ -9,11 +9,13 @@ public class MultiplicationResult {
     private final User user;
     private final Multiplication multiplication;
     private final int result;
+    private final boolean correct;
 
     public MultiplicationResult() {
         user = null;
         multiplication = null;
         result = -1;
+        correct = false;
     }
 }
 
