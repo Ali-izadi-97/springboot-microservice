55cd07be2de0b873649480ddf83fbe84e9cd474f
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-25 16:40:39 +0330

    set user as entity

--- core-v1.0/src/main/java/com/microservice/multiplication/domain/User.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/domain/User.java
@@ -3,9 +3,19 @@ package com.microservice.multiplication.domain;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @RequiredArgsConstructor
+@Entity
 public final class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
+    private Long id;
     private final String name;
 
     protected User() {
