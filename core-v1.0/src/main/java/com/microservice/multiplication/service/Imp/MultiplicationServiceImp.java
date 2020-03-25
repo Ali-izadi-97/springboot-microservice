daee69cfe047fbd021a80c80ba9b168349c18bf6
Ali-izadi-97 <izadi.ali.1997@gmail.com>
2020-03-25 20:43:55 +0330

    add method for retrieve user activation

--- core-v1.0/src/main/java/com/microservice/multiplication/service/Imp/MultiplicationServiceImp.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/service/Imp/MultiplicationServiceImp.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -65,5 +66,10 @@ public class MultiplicationServiceImp implements MultiplicationService {
                 resultModel.getResult(), correct);
         resultRepository.save(checkedResult);
         return correct;
+    }
+
+    @Override
+    public List<MultiplicationResult> getStatusForUser(String userName) {
+        return resultRepository.findTop5ByUserNameOrderByIdDesc(userName);
     }
 }
--- core-v1.0/src/main/java/com/microservice/multiplication/service/MultiplicationService.java
+++ core-v1.0/src/main/java/com/microservice/multiplication/service/MultiplicationService.java
@@ -3,7 +3,10 @@ package com.microservice.multiplication.service;
 import com.microservice.multiplication.domain.Multiplication;
 import com.microservice.multiplication.domain.MultiplicationResult;
 
-public interface    MultiplicationService {
+import java.util.List;
+
+public interface MultiplicationService {
     Multiplication createRandomMultiplication();
     boolean checkAttempt(final MultiplicationResult resultAttempt);
+    List<MultiplicationResult> getStatusForUser(String userName);
 }
